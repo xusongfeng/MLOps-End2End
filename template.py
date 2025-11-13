e9298ab1400a300cbdfdad03d852412720a2fac4
Bable Xu <songfxu@cisco.com>
2025-11-13 15:34:38 +0800

    project structure

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+project_name = 'datascience'
+list_of_files = [
+    '.github/workflows/.gitkeep',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/utils/common.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/entity/config_entity.py',
+    f'src/{project_name}/constants/__init__.py',
+    'params.yaml',
+    'schema.yaml',
+    'main.py',
+    'Dockerfile',
+    'setup.py',
+    'research/research.ipynb',
+    'templates/inde,html'
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != '':
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Create directory {filedir} for the file: {filename}')
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f'Create empty file: {filepath}')
+    else:
+        logging.info(f'{filepath} is already existed')
